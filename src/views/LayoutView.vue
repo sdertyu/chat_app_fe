f16956ed05de7c21b09b9850f28b9d8eadbc7686
PNgo <114852769+sdertyu@users.noreply.github.com>
2025-05-17 01:16:09 +0700

    read message

--- src/views/LayoutView.vue
+++ src/views/LayoutView.vue
@@ -22,6 +22,9 @@
                         <div class="message-bubble" :class="{ 'own': message.sender === 'self' }">
                             {{ message.content }}
                         </div>
+                        <div>
+                            {{ message.readUser }}
+                        </div>
                     </div>
                 </div>
                 <div class="chat-image-preview" v-if="currentChat.imagePreview">
@@ -29,8 +32,14 @@
                 </div>
             </div>
 
+            {{ isTyping }}
+            <div v-if="isTyping" class="typing-indicator">
+                Ai đó đang nhập...
+            </div>
+
+
             <!-- Footer Component -->
-            <FooterView v-model:newMessage="newMessage" @send-message="sendMessage" />
+            <FooterView v-model:newMessage="newMessage" @send-message="sendMessage" @input="handleTyping" />
         </div>
 
         <!-- Profile Sidebar Component -->
@@ -39,7 +48,7 @@
 </template>
 
 <script setup lang="ts">
-import { ref, computed, onMounted, nextTick } from 'vue';
+import { ref, computed, onMounted, nextTick, watch, onBeforeUnmount } from 'vue';
 import SidebarView from '@/components/layout/SidebarView.vue';
 import HeaderView from '@/components/layout/HeaderView.vue';
 import FooterView from '@/components/layout/FooterView.vue';
@@ -50,9 +59,11 @@ import socket from '@/plugins/socket';
 
 // Types
 export interface Message {
+    id: number;
     sender: 'self' | 'other' | 'system';
     content: string;
     avatar?: string;
+    readUser?: []
 }
 
 export interface Chat {
@@ -64,6 +75,7 @@ export interface Chat {
     unreadCount: number;
     messages: Message[];
     imagePreview?: string;
+    members?: Member[];
 }
 
 export interface Member {
@@ -83,6 +95,8 @@ const searchQuery = ref('');
 const selectedChatId = ref(1);
 const newMessage = ref('');
 const messagesContainer = ref<HTMLElement | null>(null);
+const isTyping = ref(false);
+
 
 // localStorage
 const userId = localStorage.getItem('userId');
@@ -93,6 +107,13 @@ const settings = ref<Settings>({
     sound: false,
     saveDownloads: false
 });
+
+const handleTyping = (): void => {
+    socket.emit('typing', {
+        senderId: userId,
+        conversationId: String(selectedChatId.value)
+    });
+};
 
 // Sample data
 const chats = ref<Chat[]>([]);
@@ -162,16 +183,25 @@ const chats = ref<Chat[]>([]);
 // ]);
 
 const members = ref<Member[]>([
-    { id: 1, name: 'Robin Jonas', avatar: 'https://tamkytourism.com/wp-content/uploads/2025/02/avatar-vo-tri-9.jpg' },
-    { id: 2, name: 'Cameron Williamson', avatar: 'https://tamkytourism.com/wp-content/uploads/2025/02/avatar-vo-tri-9.jpg' },
-    { id: 3, name: 'Eleanor Pena', avatar: 'https://tamkytourism.com/wp-content/uploads/2025/02/avatar-vo-tri-9.jpg' },
-    { id: 4, name: 'Arlene McCoy', avatar: 'https://tamkytourism.com/wp-content/uploads/2025/02/avatar-vo-tri-9.jpg' },
-    { id: 5, name: 'Dianne Russell', avatar: 'https://tamkytourism.com/wp-content/uploads/2025/02/avatar-vo-tri-9.jpg' }
+    // { id: 1, name: 'Robin Jonas', avatar: 'https://tamkytourism.com/wp-content/uploads/2025/02/avatar-vo-tri-9.jpg' },
+    // { id: 2, name: 'Cameron Williamson', avatar: 'https://tamkytourism.com/wp-content/uploads/2025/02/avatar-vo-tri-9.jpg' },
+    // { id: 3, name: 'Eleanor Pena', avatar: 'https://tamkytourism.com/wp-content/uploads/2025/02/avatar-vo-tri-9.jpg' },
+    // { id: 4, name: 'Arlene McCoy', avatar: 'https://tamkytourism.com/wp-content/uploads/2025/02/avatar-vo-tri-9.jpg' },
+    // { id: 5, name: 'Dianne Russell', avatar: 'https://tamkytourism.com/wp-content/uploads/2025/02/avatar-vo-tri-9.jpg' }
 ]);
 
 // Computed properties
 const currentChat = computed((): Chat => {
     return chats.value.find(chat => chat.id === selectedChatId.value) || chats.value[0] || [];
+});
+
+watch(selectedChatId, (newId) => {
+    if (socket.connected) {
+        let lastMessageId = currentChat.value.messages.length > 0
+            ? currentChat.value.messages[currentChat.value.messages.length - 1].id
+            : null;
+        socket.emit('join_room', { conversationId: String(newId), lastMessageId: Number(lastMessageId), senderId: Number(userId) });
+    }
 });
 
 // Methods
@@ -193,46 +223,11 @@ const sendMessage = (): void => {
     if (!newMessage.value.trim()) return;
 
     socket.emit('sendMessage', {
+        id: Number(null),
         senderId: userId,
         content: newMessage.value,
         conversationId: selectedChatId.value
     });
-
-    // Simulate sending message
-    // const chat = chats.value.find(c => c.id === selectedChatId.value);
-    // if (chat) {
-    //     chat.messages.push({
-    //         sender: 'self',
-    //         content: newMessage.value
-    //     });
-    //     chat.lastMessage = newMessage.value;
-    //     chat.lastMessageTime = new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
-    //     chat.unreadCount += 1;
-    //     newMessage.value = '';
-    // }
-
-
-
-    // const chat = chats.value.find(c => c.id === selectedChatId.value);
-    // if (chat) {
-    //     chat.messages.push({
-    //         sender: 'self',
-    //         content: newMessage.value
-    //     });
-    //     chat.lastMessage = newMessage.value;
-    //     chat.lastMessageTime = new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
-    //     newMessage.value = '';
-
-    //     // Simulate response after a delay
-    //     setTimeout(() => {
-    //         chat.messages.push({
-    //             sender: 'other',
-    //             avatar: 'https://tamkytourism.com/wp-content/uploads/2025/02/avatar-vo-tri-9.jpg',
-    //             content: "Thanks for your message! I'll get back to you soon."
-    //         });
-    //         scrollToBottom();
-    //     }, 1000);
-    // }
 
     nextTick(() => {
         newMessage.value = '';
@@ -253,28 +248,48 @@ const getListChat = async (): Promise<void> => {
             chats.value = response.data.map((chat: any) => ({
                 id: chat.id,
                 name: chat.name,
-                avatar: chat.avatar,
-                lastMessage: chat.messages[0].content,
-                lastMessageTime: new Date(chat.messages[0].createdAt)
-                    .toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }),
-                unreadCount: 1,
+                avatar: chat.avatar || 'https://tamkytourism.com/wp-content/uploads/2025/02/avatar-vo-tri-9.jpg',
+                lastMessage: chat.messages?.[chat.messages.length - 1]?.content || '',
+                lastMessageTime: chat.messages?.[chat.messages.length - 1]?.createdAt
+                    ? new Date(chat.messages[chat.messages.length - 1].createdAt).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })
+                    : chat.createdAt
+                        ? new Date(chat.createdAt).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })
+                        : '',
+                unreadCount: 0,
                 messages: chat.messages.map((message: any) => ({
+                    id: message.id,
                     sender: message.senderId == userId ? 'self' : 'other',
                     content: message.content,
-                    avatar: ""
+                    avatar: "https://tamkytourism.com/wp-content/uploads/2025/02/avatar-vo-tri-9.jpg",
+                    readUser: chat.participants.map((user: any) => {
+                        if (user.lastReadMessageId == message.id) {
+                            return {
+                                id: user.usersId,
+                            };
+                        }
+                        return null; // hoặc undefined
+                    })
+
                 })),
+
             }));
         }
     } catch (error) {
         console.error('Error fetching chat list:', error);
         chats.value = [];
     }
+    finally {
+        // Scroll to bottom after fetching chats
+        nextTick(() => {
+            scrollToBottom();
+        });
+    }
 };
 
 // Lifecycle hooks
 onMounted(() => {
     getListChat();
-    scrollToBottom();
+    // scrollToBottom();
 
     socket.on('connect', () => {
         socket.emit('join_room', { conversationId: String(selectedChatId.value) });
@@ -284,9 +299,11 @@ onMounted(() => {
         const chat = chats.value.find(c => c.id == message.conversationId);
         if (chat) {
             chat.messages.push({
+                id: message.id,
                 sender: message.senderId == userId ? 'self' : 'other',
                 content: message.content,
                 avatar: ""
+
             });
 
             // Nếu là hội thoại hiện tại thì scroll xuống
@@ -299,6 +316,22 @@ onMounted(() => {
         console.log(message);
     });
 
+    socket.on('typing2', (data) => {
+        console.log("object");
+        if (data.conversationId == selectedChatId.value && data.senderId != userId) {
+            isTyping.value = true;
+
+            setTimeout(() => {
+                isTyping.value = false;
+            }, 3000); // tự ẩn sau 3s
+        }
+    });
+
+});
+
+onBeforeUnmount(() => {
+    socket.off('receive_message');
+    socket.off('connect');
 });
 </script>
 
